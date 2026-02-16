2efabce7c1a1f1841c2e45f0b661a187f4ccaf32
DeividEA <56396346+DevDeividEA@users.noreply.github.com>
2026-02-16 02:27:03 +0000

    practice

--- exercises/026-computeAreaOfATriangle/app.js
+++ exercises/026-computeAreaOfATriangle/app.js
@@ -1,1 +1,4 @@
 // Write your function here
+function computeAreaOfATriangle(base, height){
+    return 0.5 * base * height;
+}
--- exercises/028-square/app.js
+++ exercises/028-square/app.js
@@ -1,1 +1,4 @@
 // Write your function here
+function square(num1){
+    return num1 * num1;
+}
