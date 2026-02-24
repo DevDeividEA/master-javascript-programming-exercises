694ef06be96157da2b2d4651b11f36ca948d1311
DeividEA <56396346+DevDeividEA@users.noreply.github.com>
2026-02-24 10:53:02 +0000

    practice

--- exercises/079-findMinLengthOfThreeWords/app.js
+++ exercises/079-findMinLengthOfThreeWords/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+function findMinLengthOfThreeWords(word1, word2, word3) {
+    let result = Math.min(word1.length, word2.length, word3.length)
+    return result
+}
--- exercises/080-findMaxLengthOfThreeWords/app.js
+++ exercises/080-findMaxLengthOfThreeWords/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+function findMaxLengthOfThreeWords(word1, word2, word3) {
+    let result = Math.max(word1.length, word2.length, word3.length)
+    return result
+}
