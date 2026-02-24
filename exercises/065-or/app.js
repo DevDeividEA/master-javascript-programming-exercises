272b83f6d46a8801d5a291dd38d728973c538383
DeividEA <56396346+DevDeividEA@users.noreply.github.com>
2026-02-24 01:18:40 +0000

    practice

--- exercises/065-or/app.js
+++ exercises/065-or/app.js
@@ -1,6 +1,12 @@
 function or(expression1, expression2) {
   // your code here
-  
+  if (expression1){
+    return expression1
+  }
+
+  else{
+    return expression2
+  }
 }
 
 let output = or(true, false);
