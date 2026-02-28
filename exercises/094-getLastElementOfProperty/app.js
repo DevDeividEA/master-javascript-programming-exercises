e3b9ac055a45de3d5e5e44ee7b6f0d4427b48969
DeividEA <56396346+DevDeividEA@users.noreply.github.com>
2026-02-28 16:57:15 +0000

    practice

--- exercises/094-getLastElementOfProperty/app.js
+++ exercises/094-getLastElementOfProperty/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function getLastElementOfProperty(obj, key, n) {
+    if (!obj[key] || !Array.isArray(obj[key]) || n >= obj[key].length) {
+        return undefined
+    }
+    return obj[key][obj[key].length - 1]
+}
--- exercises/095-getElementsThatEqual10AtProperty/app.js
+++ exercises/095-getElementsThatEqual10AtProperty/app.js
@@ -1,1 +1,16 @@
 // Write your function here
+function getElementsThatEqual10AtProperty(obj, key) {
+    if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length == 0) {
+        return []
+    }
+
+    let arr = obj[key]
+    let newArr = []
+
+    arr.forEach(element => {
+        if (element === 10) {
+            newArr.push(element)
+        }
+    })
+    return newArr
+}
