3f5e6de7ac98a3beef867050df2010eb55da6cbd
DeividEA <56396346+DevDeividEA@users.noreply.github.com>
2026-03-02 12:01:34 +0000

    practice

--- exercises/100-getEvenLengthWordsAtProperty/app.js
+++ exercises/100-getEvenLengthWordsAtProperty/app.js
@@ -1,8 +1,10 @@
 function getEvenLengthWordsAtProperty(obj, key) {
     // your code here
+    if (!obj[key] || !Array.isArray(obj[key]) || obj[key].length % 2 === 0) {
+      return []
+    }
 
-    
-    
+    return obj[key].filter(element => element.length % 2 === 0)
 }
 
 let obj = {
